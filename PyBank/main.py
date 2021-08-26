f5bd06d339530d2a9401f7706b144b008c4af68c
BenPickles85 <btpickles85@gmail.com>
2021-08-26 12:53:21 +1000

    push final copy

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,16 +3,18 @@ import os
 import csv
 
 #Set path for file
-#csvpath = os.path.join("Resources", "budget_data.csv")
-csvpath = r"C:\Users\Guest1\Documents\Bootcamp\Homework1\python-challenge\PyBank\Resources\budget_data.csv"
+csvpath = os.path.join("Resources", "budget_data.csv")
+#csvpath = r"C:\Users\Guest1\Documents\Bootcamp\Homework1\python-challenge\PyBank\Resources\budget_data.csv"
 
+#Define values
 month_changes = []
-month_count = 0
+
 total_prof_loss = 0
 month_change = 0
 greatest_increase = 0
 greatest_decrease = 0
 
+month_count = 0
 
 with open(csvpath) as csvfile:
 
@@ -21,7 +23,6 @@ with open(csvpath) as csvfile:
 
     csv_header = next(csvreader)
     
-
     for row in csvreader:     
         
         #Count of total months
@@ -51,7 +52,7 @@ with open(csvpath) as csvfile:
         
 #Calculate average change
 total_change = sum(month_changes)
-average_change = total_change / (month_count - 1)
+average_change = round(total_change / (month_count - 1), 2)
 
 #Define whether there was a profit or a loss
 if total_prof_loss >0:
@@ -60,10 +61,21 @@ else:
     profit_or_loss = "Loss"
 
 #Print outcomes
+print('Financial Analysis\n-----------------------')
 print(f"Total Months: {month_count} months")
 print(f"Total {profit_or_loss}: ${total_prof_loss}")
-print(f"Average profit/loss change: ${average_change}")
-print(f"The month with the greatest increase is {month_of_greatest_increase} with an increase of ${greatest_increase}")
-print(f"The month with the greatest decrease is {month_of_greatest_decrease} with a decrease of ${greatest_decrease}")
+print(f"Average change: ${average_change}")
+print(f"The month with the greatest increase in profit is {month_of_greatest_increase} with an increase of ${greatest_increase}")
+print(f"The month with the greatest decrease in profit is {month_of_greatest_decrease} with a decrease of ${greatest_decrease}")
+
+textpath = os.path.join("Analysis", "Financial_Analysis.txt")
+with open(textpath, 'w') as text_file:
+    text_file.writelines('Financial Analysis\n-----------------------\n')
+    text_file.writelines(f"Total Months: {month_count} months\n")
+    text_file.writelines(f"Total {profit_or_loss}: ${total_prof_loss}\n")
+    text_file.writelines(f"Average change: ${average_change}\n")
+    text_file.writelines(f"The month with the greatest increase in profit is {month_of_greatest_increase} with an increase of ${greatest_increase}\n")
+    text_file.writelines(f"The month with the greatest decrease in profit is {month_of_greatest_decrease} with a decrease of ${greatest_decrease}\n")
     
+
 
---
+++ PyPoll/main.py
@@ -0,0 +1,75 @@
+# Modules
+import os
+import csv
+
+#Set path for file
+#csvpath = os.path.join("Resources", "budget_data.csv")
+
+csvpath = r"C:\Users\Guest1\Documents\Bootcamp\Homework1\python-challenge\PyPoll\Resources\election_data.csv"
+
+#Create dicitonary for results
+election_results = {
+}
+
+#Set vote count variable as zero
+vote_count = 0
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #Set header
+    csv_header = next(csvreader)  
+        
+    for row in csvreader:           
+    
+    #Calculate total votes
+        vote_count += 1
+    #Set dictionary key as candidate names
+        candidate = row[2]
+        election_results.setdefault(candidate, 0)
+    #Count votes for each candidate and store as value
+        election_results[candidate] += 1
+
+#Create lists from dictionary for camndidates and their vote count
+candidate_list = []
+candidate_votes = []
+
+for candidate, votes in election_results.items():
+    candidate_list.append(candidate)
+    candidate_votes.append(votes)
+
+#Calculate percentage of vote for each candidate and create a list for this
+vote_percentage = []
+
+for votes in candidate_votes:
+    vote_percentage.append(round(votes/vote_count * 100, 2))
+    
+#Create tuple of candidate name, vote percentage and votes won
+final_data = list(zip(candidate_list, vote_percentage, candidate_votes))
+
+#determine winning candidate
+winning_candidate = []
+
+for candidate in final_data:
+    if max(candidate_votes) == candidate[2]:
+        winning_candidate.append(candidate[0])
+
+#print list of candidates and results
+print('Election Results \n------------------------')
+print(f'Total Votes: {vote_count}\n------------------------')
+
+for i in range(len(final_data)):
+    print(f'{candidate_list[i]}: {vote_percentage[i]}% ({candidate_votes[i]})')
+
+print(f'------------------------\nWinner: {winning_candidate[0]}\n------------------------')
+
+
+textpath = os.path.join("Analysis", "Polling_Summary.txt")
+with open(textpath, 'w') as text_file:
+    text_file.writelines('Election Results \n------------------------\n')
+    text_file.writelines(f'Total Votes: {vote_count}\n------------------------\n')
+    for i in range(len(final_data)):
+        text_file.writelines(f'{candidate_list[i]}: {vote_percentage[i]}% ({candidate_votes[i]})\n')
+    text_file.writelines(f'------------------------\nWinner: {winning_candidate[0]}\n------------------------')
